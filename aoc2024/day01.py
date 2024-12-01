261ac45f9633c86abf057c591b54f04459b121ae
Teekeks <info@teawork.de>
2024-12-01 08:56:22 +0100

    code cleanup day 1

--- aoc2024/day01.py
+++ aoc2024/day01.py
@@ -1,19 +1,15 @@
-from aoc2024.util import get_data
-from pprint import pprint
-
 
 def process(data):
     l1 = list()
     l2 = list()
-    tally = 0
+    r1 = 0
     for entry in data:
         e1, e2 = entry.split('   ')
         l1.append(int(e1))
         l2.append(int(e2))
     for e1, e2 in zip(sorted(l1), sorted(l2)):
-        tally += abs(e1 - e2)
-    r1 = tally
-    tally = 0
+        r1 += abs(e1 - e2)
+    r2 = 0
     lookup = {}
     for e in l2:
         if e not in lookup:
@@ -21,5 +17,5 @@ def process(data):
         else:
             lookup[e] += 1
     for e in l1:
-        tally += e * lookup.get(e, 0)
-    return r1, tally
+        r2 += e * lookup.get(e, 0)
+    return r1, r2
